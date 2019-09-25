225fc92b7b01da9f8ee26dbddae14a745ea379b0
David <blittynet@gmail.com>
2019-09-25 11:21:04 +0200

    StickersDone

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ bot.onText(/^\/start/, (msg) => {
 });
 
 bot.onText(/\/sticker/, (msg) => {
-    bot.sendSticker(msg.chat.id, 'CAADAgADGQAD9wLID84FRYkfvYGOFgQ');
+    const stickers = ['CAADAgAD5gcAApb6EgVgdqDlFwZ93xYE',
+        'CAADAgAD2wcAApb6EgUg5mJ5rw9CmRYE',
+        'CAADAgADEAADr8ZRGp93zTWe_IunFgQ',
+        'CAADAgADBQADr8ZRGpWJICJ8NGY0FgQ',
+        'CAADAgADuAkAAnlc4gmn9Hy5dMSutRYE',
+        'CAADAgADJgAD9wLID7E-3Uvon6w5FgQ',
+        'CAADAgADIAAD9wLID1KiROfjtgxPFgQ',
+        'CAADAgADIQAD9wLID5x5ZIVyrNvHFgQ',
+        'CAADAgADBQADmL-ADUXD-3pCgBZkFgQ',
+        'CAADAgADBAADmL-ADW7vNvm8sjn1FgQ',
+        'CAADAgADCQADmL-ADRK8edwuVum2FgQ',
+        'CAADAgADDwADmL-ADa3Ih1FEY1UeFgQ',
+        'CAADAgADtgkAAnlc4gnGTnKNypclSBYE',
+        'CAADAgADEQADwDZPEw2qsw_cHj7lFgQ',
+        'CAADAgADBgADwDZPE8fKovSybnB2FgQ',
+        'CAADAgADEwADwDZPE6qzh_d_OMqlFgQ',
+        'CAADAgADAwADwDZPE6Qp7uZ988B4FgQ',
+        'CAADAgADCAADwDZPE29sJgveGptpFgQ',
+        'CAADAgADCgADwDZPE_8Nrj7oDv0IFgQ',
+        'CAADAgADHwADwDZPE-Q4M_eEUpmSFgQ',
+        'CAADAgADDQADr8ZRGj7pvDy3DEgWFgQ'];
+    let x = Math.floor(Math.random() * Math.floor(stickers.length));
+    bot.sendSticker(msg.chat.id, stickers[x]);
 });
 
 bot.onText(/\/help/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Los comandos que puedes usar son:\n\t- /start\n\t- /hola\n\t- /sticker\n\t- /anios [año en que naciste]");
+    bot.sendMessage(msg.chat.id, "Los comandos que puedes usar son:\n\t- /start\n\t- /hola\n\t- /sticker\n\t- /anios [año en que naciste]\n\t- /operar [num1][+-/*][num2]");
 });
 
 bot.onText(/\/anios (.+)/, (msg) => {
@@ -23,18 +45,34 @@ bot.onText(/\/anios (.+)/, (msg) => {
     bot.sendMessage(msg.chat.id, `Tienes ${date.getFullYear()-msg.text.replace("/anios ", "")} años`);
 });
 
-bot.onText(/^\/suma (.+)/, (msg, num) => {
-    let op;
+bot.onText(/^\/operar (.+)/, (msg, num) => {
+    //let op;
     let res;
     let numbers;
 
     if (num[1].includes('+')){
-        op = '+';
+        //op = '+';
+        numbers = num[1].split('+');
+        res = parseInt(numbers[0]) + parseInt(numbers[1]);
+        console.log(numbers);
     } else if (num[1].includes('-')){
-        op = '-';
+        //op = '-';
+        numbers = num[1].split('-');
+        res = parseInt(numbers[0]) - parseInt(numbers[1]);
+        console.log(numbers);
+    } else if (num[1].includes('*')) {
+        //op = '+';
+        numbers = num[1].split('*');
+        res = parseInt(numbers[0]) * parseInt(numbers[1]);
+        console.log(numbers);
+    } else if (num[1].includes('/')) {
+        //op = '-';
+        numbers = num[1].split('/');
+        res = parseInt(numbers[0]) / parseInt(numbers[1]);
+        console.log(numbers);
     }
 
-    switch(op) {
+    /*switch(op) {
         case '+':
             numbers = num[1].split('+');
             res = parseInt(numbers[0]) + parseInt(numbers[1]); 
@@ -44,7 +82,15 @@ bot.onText(/^\/suma (.+)/, (msg, num) => {
             numbers = num[1].split('-');
             res = parseInt(numbers[0]) - parseInt(numbers[1]);
             break;
-    }
+        case '*':
+            numbers = num[1].split('*');
+            res = parseInt(numbers[0]) - parseInt(numbers[1]);
+            break;
+        case '/':
+            numbers = num[1].split('/');
+            res = parseInt(numbers[0]) - parseInt(numbers[1]);
+            break;
+    }*/
 
     bot.sendMessage(msg.chat.id, `La operación da el resultado: ${res}`);
 });
